aea20b4cb27c24ae5b25e7641c2f341fbb606c72
AyshaShafiq <42186490+AyshaShafiq@users.noreply.github.com>
2020-08-05 17:41:53 +0530

    Add files via upload
    Question2

---
+++ question-2.js
@@ -0,0 +1,36 @@
+const addsTo = (arr,target) =>{
+	let resultArray =[];
+	let count;
+	for(let i = 0; i< arr.length - 1; i++){ 
+		 count = 0; 
+		do{
+			let sum = arr[i] + arr[count+1];
+			if(sum === target){
+				resultArray.push([arr[i],arr[count+1]]);
+			}
+			count++;
+		}while(count < arr.length -1 && arr[count+1] !== arr[count+2])
+		
+	}
+	resultArray.forEach((item,index)=> item.sort())
+	resultArray = resultArray.sort();
+	console.log("resultArray",resultUnique(resultArray));
+
+
+// to remove duplicates
+
+	function resultUnique(arr) {
+    var uniques = [];
+    var itemsFound = {};
+    for(var i = 0, l = arr.length; i < l; i++) {
+        var stringified = JSON.stringify(arr[i]);
+        if(itemsFound[stringified]) { continue; }
+        uniques.push(arr[i]);
+        itemsFound[stringified] = true;
+    }
+   return uniques;
+}
+
+}
+
+addsTo([1,2,3,1,2,3,4,5,6,4,7], 8);
